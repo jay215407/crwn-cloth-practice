e77b53986a34885db5a0aa9a5950ffd82dab5e08
Jayesh <jay.215407@gmail.com>
2020-08-30 18:07:45 +0530

    Added router feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import './App.css';
 import HomePage from '../src/pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
 
+import './App.css';
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <HomePage />      
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route  path="/hats" component={HatsPage} />
+      </Switch>
+      
+      {/* <HomePage />       */}
     </div>
   );
 }
