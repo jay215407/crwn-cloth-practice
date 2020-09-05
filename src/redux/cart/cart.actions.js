fae8beefed3843134eb8f1ffdbcf76ce65fdea76
Jayesh <jay.215407@gmail.com>
2020-09-05 13:20:14 +0530

    modified:   src/components/collection-item/collection-item.component.jsx 	modified:   src/components/collection-item/collection-item.styles.scss 	modified:   src/components/collection-preview/collection-preview.component.jsx 	modified:   src/components/custom-button/custom-button.component.jsx 	modified:   src/components/custom-button/custom-button.styles.scss 	modified:   src/pages/shop/shop.data.js 	modified:   src/redux/cart/cart.actions.js 	modified:   src/redux/cart/cart.reducer.js 	modified:   src/redux/cart/cart.types.js 	new file:   src/redux/cart/cart.utils.js

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -4,4 +4,11 @@ export const toggleCartHidden = () => {
     return {
         type: CartActionTypes.TOGGLE_CART_HIDDEN
     }
-}
+};
+
+export const addItem = item => {
+    return {
+        type: CartActionTypes.ADD_ITEM,
+        payload: item
+    }
+};
