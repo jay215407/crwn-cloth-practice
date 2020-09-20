e4e87c3738ab33702607eeb2ef4917d5b5850c2b
Jayesh <jay.215407@gmail.com>
2020-09-20 10:14:56 +0530

    Redusx logging will be applied on Dev env only and not on prod

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,11 @@ import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger] // The middleware that store is expecting from redux is in form of array.
+const middlewares = [] // The middleware that store is expecting from redux is in form of array.
+
+if(process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
